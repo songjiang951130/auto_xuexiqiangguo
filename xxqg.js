e1f0cea3bb25ba838b8f697dc2e7c08c8a439a95
songjiang <songjiangcq@qq.com>
2022-08-06 11:18:19 +0800

    处理访问异常

--- xxqg.js
+++ xxqg.js
@@ -941,44 +941,47 @@ function do_periodic_answer(number) {
  * 处理访问异常
  */
 function handling_access_exceptions() {
-    if (text("访问异常").exists()) {
-        // 滑动按钮位置
-        className('android.view.View').depth(10).clickable(true).waitFor();
-        var pos = className('android.view.View').depth(10).clickable(true).findOnce(1).bounds();
-        // 滑动框右边界
-        className('android.view.View').depth(9).clickable(false).waitFor();
-        var right_border = className('android.view.View').depth(9).clickable(false).findOnce(0).bounds().right;
-        while (text("访问异常").exists() || text("刷新").exists()) {
-            // 位置取随机值
-            var ex = (pos.left + pos.right) / 2;
-            var ey = (pos.top + pos.bottom) / 2;
-            swipe(ex, ey, ex + right_border, ey, random(500, 800));
-            press(ex + right_border, ey, 1000);
-            sleep(500);
-            // 需要开启新线程获取控件
-            threads.start(function () {
-                if (text("刷新").exists()) {
-                    click('刷新');
-                }
-            });
+    // 在子线程执行的定时器，如果不用子线程，则无法获取弹出页面的控件
+    var thread_handling_access_exceptions = threads.start(function () {
+        while (true) {
+            textContains("访问异常").waitFor();
+            // 滑动按钮">>"位置
+            idContains("nc_1_n1t").waitFor();
+            var bound = idContains("nc_1_n1t").findOne().bounds();
+            // 滑动边框位置
+            text("向右滑动验证").waitFor();
+            var slider_bound = text("向右滑动验证").findOne().bounds();
+            // 通过更复杂的手势验证（先右后左再右）
+            var x_start = bound.centerX();
+            var dx = x_start - slider_bound.left;
+            var x_end = slider_bound.right - dx;
+            var x_mid = (x_end - x_start) * random(5, 8) / 10 + x_start;
+            var back_x = (x_end - x_start) * random(2, 3) / 10;
+            var y_start = random(bound.top, bound.bottom);
+            var y_end = random(bound.top, bound.bottom);
+            x_start = random(x_start - 7, x_start);
+            x_end = random(x_end, x_end + 10);
+            gesture(random_time(delay_time), [x_start, y_start], [x_mid, y_end], [x_mid - back_x, y_start], [x_end, y_end]);
+            sleep(random_time(delay_time));
+            if (textContains("刷新").exists()) {
+                click("刷新");
+                continue;
+            }
+            if (textContains("网络开小差").exists()) {
+                click("确定");
+                continue;
+            }
+            // 执行脚本只需通过一次验证即可，防止占用资源
+            break;
         }
-        // 执行脚本只需通过一次验证即可，通过后将定时器关闭
-        threads.shutDownAll();
-    }
-    if (textContains("网络开小差").exists()) {
-        click('确定');
-    }
+    });
+    return thread_handling_access_exceptions;
 }
 
 /* 
 处理访问异常，滑动验证
 */
-var id_handling_access_exceptions;
-// 在子线程执行的定时器，如果不用子线程，则无法获取弹出页面的控件
-var thread_handling_access_exceptions = threads.start(function () {
-    // 每2秒就处理访问异常
-    id_handling_access_exceptions = setInterval(handling_access_exceptions, 4000);
-});
+var thread_handling_access_exceptions = handling_access_exceptions();
 
 /*
 **********每日答题*********
