3151ace745d21638fd2e15006400b27fd35f7fae
songjiang <songjiangcq@qq.com>
2022-10-03 15:35:55 +0800

    更换文案

--- question_search.js
+++ question_search.js
@@ -7,7 +7,7 @@ question_search.getAnswerText = function (question) {
     if (question == undefined || question == null || question == '') {
         return null;
     }
-    // log("question raw:" + question);
+    // log("searh question-raw:" + question);
 
     //qustion 选取文本最多的
     question = question.replace(/\d\./, "");
@@ -24,13 +24,13 @@ question_search.getAnswerText = function (question) {
     // 此网站只支持十个字符的搜索
     var r1 = http.get('http://www.syiban.com/search/index/init.html?modelid=1&q=' + encodeURI(question.slice(0, 10)));
     result = r1.body.string().match(/答案：.*</);
-    // log("question:" + question + " r1:" + result);
+    // log("searh question:" + question + " r1:" + result);
     if (result == null) {
         log("url:" + 'http://www.syiban.com/search/index/init.html?modelid=1&q=' + encodeURI(question.slice(0, 10)));
     } else {
         var result = result[0].slice(5, result[0].indexOf('<'));
         result = result.replace(/、/, "");
-        // log('答案 site: ' + result);
+        // log('searh res: ' + result);
         return result;
     }
 }
