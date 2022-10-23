9344a078cb1dbcccbc85d6def363c11858021cf6
Dream <songjiang951130@gmail.com>
2022-10-23 23:11:05 +0800

    l

--- log.js
+++ log.js
@@ -1,10 +1,11 @@
 var my_log = {};
 
-my_log.config = function () {
+my_log.config = function() {
     var d = new Date();
     var dateString = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate();
+    log("name:" + dateString);
     console.setGlobalLogConfig({
         "file": "./logs/" + "xxqg_" + dateString + ".log",
-    })
+    });
 }
 module.exports = my_log;
